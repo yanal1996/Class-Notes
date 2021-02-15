842d30e84e4ddfef9d268ba8ceda21de22849743
Yanal Shihab Aldeen <yshehabaldeen51@gmail.com>
2021-02-15 23:26:49 +0200

    class 2 lab

---
+++ 2/js/app.js
@@ -0,0 +1,28 @@
+    var name = prompt("please enter your name")
+      alert("welcome " + name + " lets see is you know 5 things about me")
+      var ans1 = prompt("I lived in the US");
+      if (ans1 = true) {
+      alert("true, I lived in Indiana for 5 years");
+      
+      }
+      var ans2 = prompt("I enjoy boxing");
+      if (ans2 = true) {
+      alert("true, I have been boxing since 2019");
+    
+      }
+      var ans3 = prompt("I love mlukheye");
+      if (ans3 != true) {
+      alert("false, I hate mlukheye");
+      
+      } 
+      var ans4 = prompt("My dog is called Nairobi");
+      if (ans4 = true) {
+      alert("true, and shes amazing <3");
+      }
+      var ans5 = prompt("I have tattoos");
+      if (ans5 = true) {
+      alert("true, I have 2 on my right arm");
+      
+      }
+
+
