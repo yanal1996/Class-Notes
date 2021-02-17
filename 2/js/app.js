cd62461e0c5755efc912708072f60aa79396c5df
Yanal Shihab Aldeen <yshehabaldeen51@gmail.com>
2021-02-18 00:53:12 +0200

    lab done

--- 2/js/app.js
+++ 2/js/app.js
@@ -3,36 +3,42 @@ function greeting (){
   let name = prompt('please enter your name');
   alert('welcome ' + name + ' lets see is you know 5 things about me');
 }
+let score = 0
 function q1(){
   let ans1 = prompt('I lived in the US');
   if (ans1 === 'yes' || ans1 === 'y') {
-  alert('true, I lived in Indiana for 5 years');
+  alert('true, I lived in Indiana for 5 years')
+  let score = score +1;
   }
 }
 function q2(){
   let ans2 = prompt('I enjoy boxing');
   if (ans2 === 'yes' || ans2 === 'y') {
-  alert('true, I have been boxing since 2019');
+  alert('true, I have been boxing since 2019')
+  let score = score + 1;
 
   }
 }
 function q3(){
   let ans3 = prompt('I love mlukheye');
   if (ans3 === 'no' || ans3 === 'n') {
-  alert('false, I hate mlukheye');
+  alert('false, I hate mlukheye')
+  let score = score + 1;
   
   } 
 }
 function q4(){
   let ans4 = prompt('My dog is called Nairobi');
   if (ans4 === 'yes' || ans4 === 'y') {
-  alert('true, and shes amazing <3');
+  alert('true, and shes amazing <3')
+  let score = score + 1;
   }
 }
 function q5(){
   let ans5 = prompt('I have tattoos');
   if (ans5 === 'yes' || ans5 === 'y') {
-  alert('true, I have 2 on my right arm');
+  alert('true, I have 2 on my right arm')
+  let score = score + 1;
   
   }
 }
@@ -41,7 +47,8 @@ function q6(){
   while(trial<5){
     let num = prompt('guess a number');
     let trial = trial + 1
-    if (num = 69){alert('correct')}
+    if (num = 69){alert('correct') 
+    let score = score + 1}
     else if (num > 69){alert('too high')}
     else if(num < 69){alert('too low')};
   }
@@ -51,7 +58,8 @@ function q6(){
       let array = ['Dana', 'Sara', 'Ahmad']
       let ans7 = prompt('whats one of my siblings names?')
       if (ans7 = array){alert('correct')
-      let trial = trial + 20}
+      let trial = trial + 20
+      let score = score + 1}
       else if (ans7 != array){trial = trial + 1}
       alert(array)
 
@@ -67,3 +75,4 @@ q4();
 q5();
 q6();
 q7();
+alert('you scored ' + score + ' out of 7');
